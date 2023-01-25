b8648bd7f8aa481b9f0c43fd711d24de9a1f8ad7
abimanyu-abi <abisquare0215@gmail.com>
2023-01-25 09:18:17 +0530

    create.js functionality added

--- src/create.js
+++ src/create.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+
+    const [title,setTitle]=useState("");
+    const [body,setBody]=useState("");
+    const [author,setAuthor]=useState("");
+    const handle=(e)=>{
+        e.preventDefault();
+        const blog={title,body,author};
+         
+        fetch
+
+    }
+
     return ( 
         <div className="create-blog">
-            Create a blog here
+            <h2 className="create-title">Create Amazing Blogs</h2>
+            <form className="create-form" onSubmit={handle}>
+                <label>Title of Blog :</label>
+                <input 
+                required
+                onChange={(e)=>setTitle(e.target.value)}
+                type="text"></input>
+                <label>Blog body :</label>
+                <textarea cols="30" rows="10"></textarea>
+                <label
+                required
+                onChange={(e)=>setBody(e.target.value)}
+                >Your Name :</label>
+                <input 
+                required
+                onChange={(e)=>setAuthor(e.target.value)}
+                type="text"></input>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
